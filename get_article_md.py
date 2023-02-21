74fd69dbc119c8bebd4e1896f724f4b0da8af187
zhangyue325 <zhangyue325@gmail.com>
2023-02-21 16:53:36 +0800

    update0220

--- get_article_md.py
+++ get_article_md.py
@@ -3,30 +3,35 @@ import requests
 import pandas as pd
 from markdownify import markdownify as md
 from datetime import timedelta, date
+import os 
+import shutil
+
 
 def add_article_md_foresightnews(url):
     article_md = ""
-
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
 
     for data in soup.findAll('div',{'class':'detail-body ql-editor'}):
         break
 
-    with open("test.txt", "w", encoding='utf-8-sig') as f:
-        for ele in data:
-            ele = str(ele)
-            if ele == "<p><br/></p>" or ele.startswith("<blockquote>") or ele.startswith("<p><strong>"):
-                continue
+    print("get article md foresightnews")
+    for ele in data:
+        ele = str(ele)
+        if ele == "<p><br/></p>" or ele.startswith("<blockquote>") or ele.startswith("<p><strong>"):
+            continue
 
-            try:
-                ele = md(ele)
-                article_md += ele
-            except:
-                pass
+        try:
+            ele = md(ele)
+            article_md += ele
+        except:
+            pass
     return article_md
 
 
 def add_all_articles_md(dt):
+    shutil.rmtree('full_articles')
+    os.mkdir('full_articles')
+
     df = pd.read_csv(rf"trending_articles\{dt}.csv")
     n = len(df)
 
@@ -34,7 +39,15 @@ def add_all_articles_md(dt):
         if df.loc[i, "source"] == "foresightnews":
             url = df.loc[i, "url"]
             article_md = add_article_md_foresightnews(url)
-            df.loc[i, "article"] = article_md
+            # df.loc[i, "article"] = article_md
+
+            with open(rf"full_articles\{i}.txt", "w+", encoding="utf-8") as f:
+                f.write(article_md)
 
     df.to_csv(rf"trending_articles\{dt}.csv", index = False)
 
+
+
+if __name__ == "__main__":
+    yesterday = date.today() + timedelta(days = -1)
+    add_all_articles_md(yesterday)
